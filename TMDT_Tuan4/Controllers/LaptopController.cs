5f13011882c19370d13ee0a6d45d558c44ba5da5
Administrator <Administrator@PC005>
2020-10-30 11:20:22 +0700

    search Weight

--- TMDT_Tuan4/Controllers/LaptopController.cs
+++ TMDT_Tuan4/Controllers/LaptopController.cs
@@ -24,5 +24,55 @@ namespace TMDT_Tuan4.Controllers
             var ListCPU = db.laptops.GroupBy(l => l.Cpu).Select(l => l.Key);
             return PartialView(ListCPU);
         }
+        public ActionResult getScreenResolution()
+        {
+            var ListScreenResolution = db.laptops.GroupBy(l => l.ScreenResolution).Select(l => l.Key);
+            return PartialView(ListScreenResolution);
+        }
+        public ActionResult getTypeName()
+        {
+            var ListTypeName = db.laptops.GroupBy(l => l.TypeName).Select(l => l.Key);
+            return PartialView(ListTypeName);
+        }
+        public ActionResult getMemory()
+        {
+            var ListMemory = db.laptops.GroupBy(l => l.Memory).Select(l => l.Key);
+            return PartialView(ListMemory);
+        }
+        public ActionResult getGPU()
+        {
+            var ListGPU = db.laptops.GroupBy(l => l.Gpu).Select(l => l.Key);
+            return PartialView(ListGPU);
+        }
+        public ActionResult getOS()
+        {
+            var ListOS = db.laptops.GroupBy(l => l.OpSys).Select(l => l.Key);
+            return PartialView(ListOS);
+        }
+        public ActionResult getInches()
+        {
+            var ListInches = db.laptops.GroupBy(l => l.Inches).Select(l => l.Key);
+            return PartialView(ListInches);
+        }
+        public ActionResult getSpeed()
+        {
+            var ListSpeed = db.laptops.GroupBy(l => l.Speed).Select(l => l.Key);
+            return PartialView(ListSpeed);
+        }
+        public ActionResult getWeight()
+        {
+            var ListWeight = db.laptops.GroupBy(l => l.Weight).Select(l => l.Key);
+            return PartialView(ListWeight);
+        }
+        public ActionResult getPriceLow()
+        {
+            var ListPriceLow = db.laptops.GroupBy(l => l.Price_euros).Select(l => l.Key);
+            return PartialView(ListPriceLow);
+        }
+        public ActionResult getPriceHigh()
+        {
+            var ListPriceHigh = db.laptops.GroupBy(l => l.Price_euros).Select(l => l.Key);
+            return PartialView(ListPriceHigh);
+        }
     }
 }
